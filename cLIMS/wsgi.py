f000ec988e967e781b2959185db847d5108b53ad
nanda <nanda@ankitas-mbp.ad.umassmed.edu>
2016-10-26 10:33:52 -0400

    New Workspace

--- cLIMS/wsgi.py
+++ cLIMS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for orm project.
+WSGI config for cLIMS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cLIMS.settings")
 
 application = get_wsgi_application()
