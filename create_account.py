48882546afee41fc2901df6daebb128b8711c14b
nanda <nanda@ankitas-mbp.ad.umassmed.edu>
2016-12-21 11:00:53 -0500

    create account fix

--- create_account.py
+++ create_account.py
@@ -8,31 +8,35 @@ Created on Dec 20, 2016
 ###RUN COMMAND FROM SHELL###
 #python3 manage.py shell
 #import create_account
-#create_account.readAccounts("/home/ubuntu/clims_automated_emails/lab_emails.txt")
+#create_account.createAccounts("/home/ubuntu/clims_automated_emails/lab_emails.txt")
 ###########################################
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 
-def createAccounts(userEmail,userName, userPassword):
-    user = User.objects.create_user(userName, userEmail, userPassword)
-    user.save()
-    name=userEmail.split("@")
-    flName=name[0].split(".")
-    print(flName)
-    user.first_name=flName[0]
-    user.last_name=flName[1]
-    user.save()
-    group = Group.objects.get(name='Member')
-    user.groups.add(group) 
+def createAccountHelper(userEmail,userName, userPassword):
+    if(get_user_model().objects.filter(username=userName)):
+        print("WARNING!! USER ALREADY EXISTS, SKIPPING " + userName)
+    else:
+        user = get_user_model().objects.create_user(userName, userEmail, userPassword)
+        user.save()
+        name=userEmail.split("@")
+        flName=name[0].split(".")
+        print("USER CREATED "+userName)
+        user.first_name=flName[0]
+        user.last_name=flName[1]
+        user.save()
+        group = Group.objects.get(name='Member')
+        user.groups.add(group) 
     
     
-def readAccounts(accountList):
+def createAccounts(accountList):
     with open(accountList, 'r') as f:
         read_data = f.readlines()
         for line in read_data:
             userInfoArray = line.split()
-            userEmail = userInfoArray[0]
-            userName = userInfoArray[1]
+            userEmail = userInfoArray[0].lower()
+            name=userEmail.split("@")
+            userName = name[0]
             userPassword = userInfoArray[2]
-            createAccounts(userEmail,userName, userPassword)
+            createAccountHelper(userEmail,userName, userPassword)
             
