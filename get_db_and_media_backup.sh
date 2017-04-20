888d017dbfa3ae6e81d1cd6cfb069bcff80400a4
nanda <nanda@ankitas-mbp.ad.umassmed.edu>
2017-04-20 11:05:25 -0400

    aws path to local

--- get_db_and_media_backup.sh
+++ get_db_and_media_backup.sh
@@ -5,8 +5,8 @@ now="$(date +'%d-%m-%Y')"
 SQLFILE=$FOLDER/$now.sql.txt
 sudo -u postgres pg_dump -F p clims_db > $SQLFILE
 ##cp to amazon here
-/usr/local/bin/aws s3 cp $SQLFILE  s3://dekkerlab-web/db-backups/ &> /home/ubuntu/aws_backup_result.txt
+/home/ubuntu/.local/bin/aws s3 cp $SQLFILE  s3://dekkerlab-web/db-backups/ &> /home/ubuntu/aws_backup_result.txt
 #rm the file
 rm $SQLFILE
 #BACKUP the media files
-/usr/local/bin/aws s3 sync /djangoProject/cLIMS/media s3://dekkerlab-web/media-backups
+/home/ubuntu/.local/bin/aws s3 sync /djangoProject/cLIMS/media s3://dekkerlab-web/media-backups
