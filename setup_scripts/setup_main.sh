5e6b7bbe5aae62b21b91d17c501534a7ac2353a5
nanda <nanda@ankitas-mbp.ad.umassmed.edu>
2017-08-17 15:20:12 -0400

    setup scripts update

--- setup_scripts/setup_main.sh
+++ setup_scripts/setup_main.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./ubuntu1604_install_requirements.sh
-./local_postgresql_setup.sh
-./download_from_git_config_apache2.sh
+/djangoProject/cLIMS/setup_scripts/ubuntu1604_install_requirements.sh
+/djangoProject/cLIMS/setup_scripts/local_postgresql_setup.sh
+/djangoProject/cLIMS/setup_scripts/download_from_git_config_apache2.sh
